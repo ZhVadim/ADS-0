7c466545ca8f49ccdae0cddca2e66e0051dcf2e3
Вадим Желтов <99135277+ZhVadim@users.noreply.github.com>
2022-02-14 23:08:22 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,5 +1,15 @@
 // Copyright 2021 NNTU-CS
 
 int gcd(int a, int b) {
-    // поместить сюда текст реализации алгоритма (на С++)
+    int c = 0;
+    while (a != 0 && b != 0) {
+        if (a > b) {
+            a = a % b;
+        }
+        else {
+            b = b % a;
+        }
+    }
+    c = a + b;
+    return (c);
 }
